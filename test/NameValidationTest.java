338045b9c503b4b164c8685084301df825331c37
Eirik Kvam <eirik.engen.kvam@gmail.com>
2023-03-20 21:41:40 +0100

    dcscdcs

--- test/NameValidationTest.java
+++ test/NameValidationTest.java
@@ -9,24 +9,16 @@ import org.junit.Test;
 
 public class NameValidationTest extends FormValidation{
 
-
-    private String firstName = ""; //Setter navnet til å være en tom streng
-
-    Contact Contact = new Contact(this.firstName,"Nordmann", null, null, null);
-
+    Contact Contact = new Contact("","Nordmann", null, null, null);
     private boolean ExpectedValue = isValidName(Contact.getFirstName()); // Lager en boolean som returnerer en True eller False verdi, basert på hvorvidt navnet er gyldig
 
-
-    private String firstName2 = "Ola";
-    Contact Contact2 = new Contact(this.firstName2,"Nordmann", null, null, null);
+    Contact Contact2 = new Contact("Ola","Nordmann", null, null, null);
     private boolean ExpectedValue2 = isValidName(Contact2.getFirstName());
 
 
     @Test
     public void testIsValidName() throws Exception {
-
         assertEquals(ExpectedValue, false); //Sjekker om navnet er ugyldig
         assertEquals(ExpectedValue2, true); //Sjekker om navnet er gyldig
-
     }
 }
