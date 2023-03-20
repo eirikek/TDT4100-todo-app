8eb6d699c562d44f2341c649674f9bb9d480574f
Fredrik <fredrch@stud.ntnu.no>
2023-03-20 13:32:27 +0100

    litt-nye-ting

---
+++ test/EditContactControllerTest.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+public class EditContactControllerTest {
+    @Test
+    public void testCancelChanges() {
+
+    }
+
+    @Test
+    public void testGetAddressTextField() {
+
+    }
+
+    @Test
+    public void testGetBirthDatePicker() {
+
+    }
+
+    @Test
+    public void testGetEmailTextField() {
+
+    }
+
+    @Test
+    public void testGetFirstNameTextField() {
+
+    }
+
+    @Test
+    public void testGetLastNameTextField() {
+
+    }
+
+    @Test
+    public void testSaveChanges() {
+
+    }
+
+    @Test
+    public void testSetMainController() {
+
+    }
+
+    @Test
+    public void testSetSelectedContact() {
+
+    }
+}
---
+++ test/FormsTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class FormsTest {
+    @Test
+    public void testIsValidAdress() {
+
+    }
+
+    @Test
+    public void testIsValidBirth() {
+
+    }
+
+    @Test
+    public void testIsValidEmail() {
+
+    }
+
+    @Test
+    public void testIsValidName() {
+
+    }
+}
