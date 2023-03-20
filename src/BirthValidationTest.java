e38a62587e007fff28511a60b71fd3ed4d38872c
Fredrik <fredrch@stud.ntnu.no>
2023-03-20 15:06:39 +0100

    testsfsf

---
+++ src/BirthValidationTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import java.time.LocalTime;
+
+public class BirthValidationTest {
+
+
+    private LocalTime birthDayBefore = LocalTime();
+    Contact Contact = new Contact("Ola","Nordmann", null, null, null);
+
+
+
+    @Test
+    public void testIsValidBirth() {
+
+    }
+}
