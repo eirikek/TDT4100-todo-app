ad3d04be3d401ac31b589d6fea37ae3fcfa36bd6
Eirik Kvam <eirik.engen.kvam@gmail.com>
2023-04-13 13:18:01 +0200

    efhjefhje

--- src/test/java/kontaktlisteapp/testIsValidName.java
+++ src/test/java/kontaktlisteapp/testIsValidName.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class testIsValidName {
+public class TestIsValidName {
 
     private Form Form ;
 
